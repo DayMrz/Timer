cf42c4acd8711ee7095f9e800abe4d1c28626b74
Leily <leilymrz@gmail.com>
2021-04-19 07:05:31 -0700

    updaiting and cleaning JS files

--- index.js
+++ index.js
@@ -1,46 +1,19 @@
-class Timer {
-    constructor(durationInput, startButton, pauseButton) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-    start = () => {
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
-    };
-
-    pause = () => {
-        clearInterval(this.interval);
-    }
-
-    tick = () => {
-        // const timeRemaining = this.timeRemaining;
-        this.timeRemaining = this.timeRemaining - 1;
-        // console.log('tick');
-    };
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-
-    // importantMethodToCall() {
-    //     console.log('important thing was done');
-    // }
-}
-
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log('Timer started')
+    },
+    onTick() {
+        console.log('Timer just ticked down')
+    },
+    onComplete() {
+        console.log('Timer is completed')
+    }
+
+});
 
 
 
